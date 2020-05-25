82de83b410470a90df04a344cd5ff9930270a1cd
HisatsuguShiine <atoh214@gmail.com>
2020-05-25 12:56:53 +0900

    add op_exec_floorf_sw_F

--- src/cpu/cpu_exec/op_exec_fpu.c
+++ src/cpu/cpu_exec/op_exec_fpu.c
@@ -3195,8 +3195,10 @@ int op_exec_floorf_sl_F(TargetCoreType *cpu)
 	if (reg3 >= CPU_GREG_NUM) {
 		return -1;
 	}
+    printf("0x%x: FLOORF.S r%d(%f),r%d(%f):%f\n",         cpu->reg.pc, reg2, reg2_data.data, reg3, reg3_data.data, result_data.data);
 	reg2_data.binary = cpu->reg.r[reg2];
 	reg3_data.binary = cpu->reg.r[reg3];
+    printf("0x%x: FLOORF.S r%d(%f),r%d(%f):%f\n",         cpu->reg.pc, reg2, reg2_data.data, reg3, reg3_data.data, result_data.data);
 
     prepare_float_op(cpu, &ex, &fpu_config);
     {
@@ -3216,7 +3218,6 @@ int op_exec_floorf_sl_F(TargetCoreType *cpu)
 
 	cpu->reg.pc += 4;
     return 0;
-
 }
 int op_exec_floorf_sul_F(TargetCoreType *cpu)
 {
@@ -3230,8 +3231,41 @@ int op_exec_floorf_suw_F(TargetCoreType *cpu)
 }
 int op_exec_floorf_sw_F(TargetCoreType *cpu)
 {
-	printf("ERROR: not supported:%s\n", __FUNCTION__);
-	return -1;
+    FpuConfigSettingType fpu_config;
+    FloatExceptionType ex;
+	uint32 reg2 = cpu->decoded_code->type_f.reg2;
+	uint32 reg3 = cpu->decoded_code->type_f.reg3;
+    FloatBinaryDataType reg2_data;
+    FloatBinaryDataType reg3_data;
+	FloatBinaryDataType result_data;
+
+	if (reg2 >= CPU_GREG_NUM) {
+		return -1;
+	}
+	if (reg3 >= CPU_GREG_NUM) {
+		return -1;
+	}
+    printf("### 1 0x%x: FLOORF.SW r%d(%f),r%d(%f):%f\n", cpu->reg.pc, reg2, reg2_data.data, reg3, reg3_data.data, result_data.data);
+	reg2_data.binary = cpu->reg.r[reg2];
+	reg3_data.binary = cpu->reg.r[reg3];
+    printf("### 2 0x%x: FLOORF.SW r%d(%f),r%d(%f):%f\n", cpu->reg.pc, reg2, reg2_data.data, reg3, reg3_data.data, result_data.data);
+
+    prepare_float_op(cpu, &ex, &fpu_config);
+    {
+        set_subnormal_operand(cpu, &fpu_config, &reg2_data);
+        result_data.data = (float)floorf(reg2_data.data);
+        set_subnormal_result(cpu, &fpu_config, &result_data);
+    }
+    end_float_op(cpu, &ex);
+	DBG_PRINT((DBG_EXEC_OP_BUF(), DBG_EXEC_OP_BUF_LEN(), "0x%x: FLOORF.SW r%d(%f),r%d(%f):%f\n", 
+        cpu->reg.pc, reg2, reg2_data.data, reg3, reg3_data.data, result_data.data));
+	cpu->reg.r[reg3] = result_data.binary;
+
+    printf("0x%x: FLOORF.SW r%d(%f),r%d(%f):%f\n", cpu->reg.pc, reg2, reg2_data.data, reg3, reg3_data.data, result_data.data);
+
+	cpu->reg.pc += 4;
+    return 0;
+
 }
 int op_exec_rsqrtf_s_F(TargetCoreType *cpu)
 {
